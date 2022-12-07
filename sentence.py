b03cc0fdd26239b82858ea6fe8303a8399e228d3
brookelove <brookelove@Brookes-iMac-2.local>
2022-12-07 12:49:52 -0800

    adding basic constructors, blocks, functions, and methods

---
+++ sentence.py
@@ -0,0 +1,18 @@
+class SentToLst():
+    def get_all_words():
+        pass
+
+    def get_word():
+        pass
+
+
+def set_word():
+    pass
+
+
+def scramble():
+    pass
+
+
+if __name__ == "_main_":
+    pass
