1edef83c640bb878ef81015dda2e4775d4446648
brookelove <brookelove@Brookes-iMac-2.hsd1.wa.comcast.net>
2022-12-07 19:22:03 -0800

    initializing a constructor

--- sentence.py
+++ sentence.py
@@ -1,4 +1,18 @@
-class SentToLst():
+import string
+
+
+class CreateList():
+
+    def __init__(self, sent=str()):
+        w_lst = []
+        self.__s = sent
+        # s_lst = list(self.__s)
+        # print(s_lst)
+        for i in self.__s:
+            if i not in string.punctuation:
+                w_lst.append(i)
+        print(w_lst)
+
     def get_all_words():
         pass
 
@@ -14,5 +28,7 @@ def scramble():
     pass
 
 
-if __name__ == "_main_":
-    pass
+if __name__ == "__main__":
+    str = "I want to eat a piece of chicken."
+    string = CreateList(str)
+    print(string)
