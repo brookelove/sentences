b45f8164e6e54b3da731f94290dfe62e48ad280c
brookelove <brookelove@Brookes-iMac-2.hsd1.wa.comcast.net>
2022-12-07 19:41:13 -0800

    starting method

--- sentence.py
+++ sentence.py
@@ -4,19 +4,34 @@ import string
 class CreateList():
 
     def __init__(self, sent=str()):
-        w_lst = []
+        # w_lst = []
         self.__s = sent
+        for i in string.punctuation:
+            self.__s = self.__s.replace(i, '')
+        # print(self.__s)
+        w_lst = self.__s.split()
+        print(w_lst)
+        # return w_lst
         # s_lst = list(self.__s)
         # print(s_lst)
-        for i in self.__s:
-            if i not in string.punctuation:
-                w_lst.append(i)
-        print(w_lst)
+        # for i in self.__s:
+        # print(i)
+        # if i not in string.punctuation:
+        # print(i)
+        # w_lst.append(i)
+        # i.pop(-1)
+        # w_lst.append(i)
 
-    def get_all_words():
-        pass
+        # w_lst.append(i)
+        # print(w_lst)
 
-    def get_word():
+    def get_all_words(self):
+        str = "I want to eat a piece of chicken."
+        string = CreateList(str)
+        # return __init__()
+        return string
+
+    def get_word(self):
         pass
 
 
@@ -29,6 +44,5 @@ def scramble():
 
 
 if __name__ == "__main__":
-    str = "I want to eat a piece of chicken."
-    string = CreateList(str)
-    print(string)
+    x = CreateList()
+    print(x.get_all_words())
